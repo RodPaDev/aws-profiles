73e2f4a2b678a3cddf9e54b9845706aa124201f2
rodapdev <47316946+RodPaDev@users.noreply.github.com>
2022-09-25 11:34:18 +0100

    use typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,15 @@
-"use strict";
-
 const path = require("path");
-const nodeExternals = require("webpack-node-externals");
-const webpack = require("webpack");
-
 module.exports = {
-  target: "node",
   entry: "./src/index.js",
-  mode: process.env.NODE_ENV || "development",
-  externals: [nodeExternals()],
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-    ],
-  },
+  target: "node",
   output: {
-    libraryTarget: "commonjs2",
-    path: path.join(__dirname, "./dist"),
-    filename: "[name].js",
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new webpack.LoaderOptionsPlugin({ minimize: true, debug: false })],
+  resolve: {
+    extensions: [".webpack.js", ".ts", ".js"],
+  },
+  module: {
+    rules: [{ test: /\.ts$/, loader: "ts-loader", exclude: /node_modules/ }],
+  },
 };
