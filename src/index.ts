2caacd91bf6812cf580800ca56d96c85c9bf2b27
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2022-09-27 21:53:48 +0100

    check for exisitng name & add new command

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import chalk from 'chalk'
 import { Command } from 'commander'
 import inquirer from 'inquirer'
+import { exit } from 'process'
 
 import AWSProfiler from './AwsProfiler'
 import { createSpaces } from './utils'
 import { Print } from './utils/Print'
 
-const defaultDirPath = AWSProfiler.getDefaultConfigDirPath()
+// const defaultDirPath = AWSProfiler.getDefaultConfigDirPath()
+const defaultDirPath =
+  '/Users/rodpadev/Desktop/projects/aws-profile-switcher/tests/config'
 const cli = new Command()
 
 cli
@@ -48,13 +51,21 @@ cli
   .action(profileName => {
     const awsProfiler = new AWSProfiler(global.path)
     const profile = awsProfiler.findCurrentProfile()
-    if (profile === profileName) {
+    const originalProfileName =
+      awsProfiler.getCaseInsensitiveProfileName(profileName)
+
+    if (!originalProfileName) {
+      Print.error('Profile does not exist')
+      exit(1)
+    }
+
+    if (profile === originalProfileName) {
       Print.info(
         chalk.white.bold('Default profile is already:'),
-        chalk.cyan(profileName)
+        chalk.cyan(originalProfileName)
       )
     } else {
-      switchProfile(awsProfiler, profileName)
+      switchProfile(awsProfiler, originalProfileName)
     }
   })
 
@@ -93,13 +104,90 @@ cli
       })
   })
 
-function switchProfile(awsProfiler: AWSProfiler, profileName: string) {
+cli
+  .command('add')
+  .description('Add a new aws profile')
+  .action(() => {
+    const awsProfiler = new AWSProfiler(global.path)
+    awsProfiler.findCurrentProfile()
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Enter a profile name: ',
+          validate: name =>
+            awsProfiler.getCaseInsensitiveProfileName(name)
+              ? 'Profile name is already in use'
+              : true
+        },
+        {
+          type: 'input',
+          name: 'aws_access_key_id',
+          message: 'aws_access_key_id: '
+        },
+        {
+          type: 'password',
+          name: 'aws_secret_access_key',
+          message: 'aws_secret_access_key: '
+        },
+        {
+          type: 'input',
+          name: 'region',
+          message: 'region: '
+        },
+        {
+          type: 'input',
+          name: 'output',
+          message: 'output: '
+        },
+        {
+          type: 'confirm',
+          name: 'setDefault',
+          message: 'Set this profile as default? ',
+          default: false
+        }
+      ])
+      .then(
+        ({
+          name,
+          aws_access_key_id,
+          aws_secret_access_key,
+          region,
+          output,
+          setDefault
+        }) => {
+          const credentials = { aws_access_key_id, aws_secret_access_key }
+          const config = { region, output }
+          awsProfiler.addProfile(credentials, config, name)
+          awsProfiler.saveProfile()
+
+          if (setDefault) {
+            switchProfile(awsProfiler, name, false)
+            Print.info(
+              'New aws profile added and set as default:',
+              chalk.cyan(chalk.cyan(name))
+            )
+          } else {
+            Print.info('New aws profile added:', chalk.cyan(chalk.cyan(name)))
+          }
+        }
+      )
+  })
+
+function switchProfile(
+  awsProfiler: AWSProfiler,
+  profileName: string,
+  print = true
+) {
   awsProfiler.setDefaultProfile(profileName)
   awsProfiler.saveProfile()
-  Print.info(
-    chalk.white('Default profile switched to: '),
-    chalk.cyan(profileName)
-  )
+  if (print) {
+    Print.info(
+      chalk.white('Default profile switched to: '),
+      chalk.cyan(profileName)
+    )
+  }
 }
 
 cli.parse()
